402a51bb4e7ee063ca98693264b91d3535095a70
Grant McGrath <grantmcgrath@me.com>
2017-09-20 14:55:10 -0400

    Really fixed travis.ci tests. Did not properly reference components in test code.

--- src/tests/BaseLayout.test.js
+++ src/tests/BaseLayout.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/BaseLayout';
+import BaseLayout from '../components/BaseLayout';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
