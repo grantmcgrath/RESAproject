f8015696fa869d7206188ca360e7e546afaa699b
Grant McGrath <grantmcgrath@me.com>
2017-09-20 14:46:34 -0400

    Fixed .travis.ci tests to properly route.

--- src/tests/Register.test.js
+++ src/tests/Register.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Register';
+import App from '../components/Register';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
