85c2496697bb37b5dd1cb730f9e83138270ed325
takaides <takaides+github@gmail.com>
2017-10-12 11:45:43 -0400

    Changed email field to username

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -10,8 +10,8 @@ export default class LogIn extends Component {
       <div id="log_in">
         <form id="log_in_form">
           <h1 id="login_title">Login</h1>
-          <input type="text" name="email" placeholder="email" className="log_in_email" />
-          <input type="password" name="password" placeholder="password" className="log_in_password" />
+          <input type="text" name="username" placeholder="Username" className="log_in_username" />
+          <input type="password" name="password" placeholder="Password" className="log_in_password" />
           <input type="submit"  value="Log In" className="submit_clear" />
         </form>
       </div>
